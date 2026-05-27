c5f0c9d755c42a2cb30e924ac332274996934308
“YenTran” <yentran.bhtech@gmail.com>
2026-05-27 10:33:36 +0700

    [IMP] Upgrade module bhs_hr_leave_alert ver 18

---
+++ bhs_hr_leave_alert/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Smart Leave Alert',
+    'version': '1.0',
+    'author': 'Bac Ha Software',
+    'website': 'https://bachasoftware.com',
+    'maintainer': 'Bac Ha Software',
+    'category': 'Human Resources',
+    'summary': "",
+    'description': "",
+    'depends': ['hr_holidays'],
+    'data': [],
+    'external_dependencies': {
+        'python': [],
+    },
+    'images': [],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3'
+}
